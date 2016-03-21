8f388e5d658e8ff0d62a5a2dfe7913d015203b6e
thomasinyvr <tomfriesman@gmail.com>
2016-03-21 13:46:58 -0700

    Initial Commit

---
+++ ScrollViewImageGalleries/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewImageGalleries
+//
+//  Created by Thomas Friesman on 2016-03-21.
+//  Copyright © 2016 Thomas Friesman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ScrollViewImageGalleries/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollViewImageGalleries
+//
+//  Created by Thomas Friesman on 2016-03-21.
+//  Copyright © 2016 Thomas Friesman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
